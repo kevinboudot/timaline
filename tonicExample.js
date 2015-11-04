7f783916c2c08e6862ca1bb4202790a2793147be
kevinboudot <kevin.boudot@gmail.com>
2015-11-04 08:00:01 +0100

    tonic Example

---
+++ tonicExample.js
@@ -0,0 +1,12 @@
+var Timaline = require('timaline');
+var timeline = new Timaline();
+
+timeline
+    .wait(500)
+    .set(function(infos){
+        console.log(infos.keyframe.real + ' ≈ ' + infos.keyframe.forecast);
+    })
+    .wait(1000)
+    .set(function(infos){
+        console.log(infos.keyframe.real + ' ≈ ' + infos.keyframe.forecast);
+    });
